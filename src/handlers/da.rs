d1a38be30a6d01d303b624cf212857b560419582
0xatomFusion <179967211+0xatomFusion@users.noreply.github.com>
2025-09-27 21:28:20 +0100

    feat: add logs

--- src/handlers/da.rs
+++ src/handlers/da.rs
@@ -37,11 +37,14 @@ pub async fn dispatch_handler(
 
     match svc.da_svc.dispatch_blob(payload.batch_number, data).await {
         Ok(resp) => Json(resp).into_response(),
-        Err(err) => (
-            axum::http::StatusCode::INTERNAL_SERVER_ERROR,
-            err.to_string(),
-        )
-            .into_response(),
+        Err(err) => {
+            tracing::error!("dispatch_blob failed: {:?}", err);
+            (
+                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                err.to_string(),
+            )
+                .into_response()
+        }
     }
 }
 
@@ -56,10 +59,13 @@ pub async fn inclusion_handler(
         })
         .into_response(),
         Ok(None) => axum::http::StatusCode::NOT_FOUND.into_response(),
-        Err(err) => (
-            axum::http::StatusCode::INTERNAL_SERVER_ERROR,
-            err.to_string(),
-        )
-            .into_response(),
+        Err(err) => {
+            tracing::error!("inclusion_handler failed: {:?}", err);
+            (
+                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                err.to_string(),
+            )
+                .into_response()
+        }
     }
 }
